c6fd3ac27806964391a692aa84b97c94225dc581
vedant khopade <pramilakhopde94597@gmail.com>
2026-08-28 00:17:46 +0530

    Add COLT dialogue

---
+++ dialogue/cutscence/cutscene_01.gd
@@ -0,0 +1,11 @@
+extends Control
+
+
+
+
+func _ready():
+	Dialogic.start("cutscene_01")
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
